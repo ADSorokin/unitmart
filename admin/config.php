d92a4e4281481a3b7d66d3d8de5a4828b3290a0f
Asorokin <sksd@mail.ru>
2016-11-16 21:28:03 +0300

    last

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/unitmart/admin/');
+define('HTTP_CATALOG', 'http://localhost/unitmart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/unitmart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/unitmart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/unitmart/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/unitmart/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/unitmart/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/unitmart/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/unitmart/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/unitmart/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/unitmart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/unitmart/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/unitmart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/unitmart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/unitmart/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/unitmart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'unitm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/unitmart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/unitmart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/unitmart/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/unitmart/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/unitmart/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/unitmart/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/unitmart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/unitmart/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/unitmart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/unitmart/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/unitmart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/unitmart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/unitmart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'unitm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
